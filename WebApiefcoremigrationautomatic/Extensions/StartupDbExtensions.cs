015ac26accd379322605e69ae77b78eee4a045b9
kartik <kartik.kumar53@gmail.com>
2024-04-05 16:13:10 +0530

    Add project files.

---
+++ WebApiefcoremigrationautomatic/Extensions/StartupDbExtensions.cs
@@ -0,0 +1,38 @@
+﻿using EFCore.AutomaticMigrations;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+using WebApiefcoremigrationautomatic.Data;
+
+namespace WebApiefcoremigrationautomatic.Extensions
+{
+    public static class StartupDbExtensions
+    {
+        public static async void CreateDbIfNotExists(this IHost host)
+        {
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+
+            var blogContext = services.GetRequiredService<BLobDbContext>();
+
+            var databsecrate = blogContext.Database.GetService<IDatabaseCreator>() 
+                as RelationalDatabaseCreator;
+
+            if (databsecrate != null)
+            {
+                if (!databsecrate.CanConnect()) databsecrate.Create();
+                if (!databsecrate.HasTables()) databsecrate.CreateTables();
+            }
+
+            //blogContext.Database.EnsureCreated();
+
+            // MigrateDatabaseToLatestVersion.Execute(blogContext);
+
+
+            //MigrateDatabaseToLatestVersion.Execute(blogContext, 
+            //   new DbMigrationsOptions { ResetDatabaseSchema = true,  });
+
+            //blogContext.MigrateToLatestVersion();
+            DBInitializerSeedData.InitializeDatabase(blogContext);
+        }
+    }
+}
